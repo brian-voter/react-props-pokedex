40a57e7675bbd00b51dcc50c1f5bef7b75b60902
Brian Voter <bvoter97@gmail.com>
2023-05-22 19:19:57 -0400

    finish main part

---
+++ src/Pokecard.js
@@ -0,0 +1,23 @@
+const POKEMON_IMAGE_URL = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/`
+
+/**
+ * Renders a card with the data for a specific pokemon
+ *
+ * @param {object} {pokemon: {id, name, type, base_experience}}
+ *
+ * @returns div with all the data provided as well as an image for the pokemon
+ */
+function Pokecard({pokemon}) {
+    const {id, name, type, base_experience} = pokemon;
+
+    return (
+        <div className="p-2 m-2 bg-light text-center w-25">
+            <b className="text-primary">{name}</b> <br/>
+            <img src={`${POKEMON_IMAGE_URL}${id}.png`} alt={name}/> <br/>
+            Type: {type} <br/>
+            EXP: {base_experience}
+        </div>
+    )
+}
+
+export default Pokecard;
